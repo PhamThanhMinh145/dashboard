437fb14b9dc2a641a1dbe55734c1798ddc297380
tthanhtung92 <55181588+tthanhtung92@users.noreply.github.com>
2022-10-31 19:07:57 +0700

    api account

--- src/components/datatable/datatableAuthor/TableAuthor.jsx
+++ src/components/datatable/datatableAuthor/TableAuthor.jsx
@@ -10,160 +10,160 @@ import Popup from "../../form/Popup";
 import Notification from "../../Notification";
 import { useRef } from "react";
 const headCells = [
-  { id: "authorID", label: "ID" },
-  { id: "authorName", label: "Author name" },
-  { id: "action", label: "Action", disableSorting: true },
+    { id: "authorID", label: "ID" },
+    { id: "authorName", label: "Author name" },
+    { id: "action", label: "Action", disableSorting: true },
 ];
 
 const TableAuthor = ({ onError }) => {
-  const [records, setRecords] = useState([]);
-  const [record, setRecord] = useState();
-  const [filterFn, setFilterFn] = useState({
-    fn: (items) => {
-      return items;
-    },
-  });
-  const [openPopup, setOpenPopup] = useState(false);
-  const [recordForEdit, setRecordForEdit] = useState(null);
-  const [notify, setNotify] = useState({
-    isOpen: false,
-    message: "",
-    type: "",
-  });
-
-  const { TblContainer, TblHead, TblPagination, recordsAfterPagingAndSorting } =
-    useTable(records, headCells, filterFn);
-
-  const url = "https://localhost:7091/Author/Get";
-
-  const fetchAuthor = () => {
-    fetch(url)
-      .then((response) => response.json())
-      .then((json) => {
-        console.log("result", json);
-        setRecords(json);
-      })
-      .catch(() => onError());
-  };
-
-  const postAuthor = (author) => {
-    fetch("https://localhost:7091/Author/Create", {
-      method: "POST",
-      headers: {
-        accept: "*/*",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        authorID: author.authorID,
-        authorName: author.authorName,
-      }),
-    })
-      .then((response) => response.json())
-      .then((json) => {
-        console.log("author add", json);
-        setRecord(json);
-      })
-      .catch(() => onError());
-  };
-
-  const putAuthor = (author) =>{
-    fetch(`https://localhost:7091/Author/Update/${author.authorID}`, {
-      method: "PUT",
-      headers: {
-        accept: "*/*",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        authorID : author.authorID,
-        authorName: author.authorName
-      }),
-    })
-      .then((response) => response.json())
-      .then((json) => {
-        console.log("author update",json);
-        setRecordForEdit(json);
-      })
-      .catch((e) => console.log(e));
-  }
-
-  const addAuthor = (author, resetForm) => {
-    if (author.authorID == 0) {
-      postAuthor(author);
-    }else{
-      putAuthor(author)
-    }
-    resetForm();
-    setRecordForEdit(null)
-    setOpenPopup(false);
-    setNotify({
-      isOpen: true,
-      message: "Submitted Successfully",
-      type: "success",
+    const [records, setRecords] = useState([]);
+    const [record, setRecord] = useState();
+    const [filterFn, setFilterFn] = useState({
+        fn: (items) => {
+            return items;
+        },
     });
-  };
+    const [openPopup, setOpenPopup] = useState(false);
+    const [recordForEdit, setRecordForEdit] = useState(null);
+    const [notify, setNotify] = useState({
+        isOpen: false,
+        message: "",
+        type: "",
+    });
 
-  useEffect(() => {
-    fetchAuthor();
-  }, [record, recordForEdit]);
+    const { TblContainer, TblHead, TblPagination, recordsAfterPagingAndSorting } = useTable(
+        records,
+        headCells,
+        filterFn
+    );
 
-  const openInPopup = (item) => {
-    setRecordForEdit(item);
-    setOpenPopup(true);
-  };
+    const url = "https://localhost:7091/Author/Get";
 
-  return (
-    <>
-      <div className="datatable">
-        <div className="title">
-          List Author
-          <Button
-            text="Add New"
-            variant="outlined"
-            startIcon={<AddIcon />}
-            size="small"
-            onClick={() => {
-              setOpenPopup(true);
-              //setRecordForEdit(null);
-            }}
-            color="addNewAuthor"
-          />
-        </div>
+    const fetchAuthor = () => {
+        fetch(url)
+            .then((response) => response.json())
+            .then((json) => {
+                console.log("result", json);
+                setRecords(json);
+            })
+            .catch(() => onError());
+    };
 
-        <TblContainer>
-          <TblHead />
-          <TableBody>
-            {recordsAfterPagingAndSorting().map((item) => (
-              <TableRow key={item.authorID} className="rowAuthor">
-                <TableCell>{item.authorID}</TableCell>
-                <TableCell>{item.authorName}</TableCell>
-                <TableCell className="action">
-                  <ActionButton
-                    color="edit"
-                    onClick={() => {
-                      openInPopup(item);
-                    }}
-                  >
-                    Edit
-                  </ActionButton>
-                  <ActionButton color="delete">Delete</ActionButton>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </TblContainer>
-        <TblPagination className="pagination" />
-      </div>
+    const postAuthor = (author) => {
+        fetch("https://localhost:7091/Author/Create", {
+            method: "POST",
+            headers: {
+                accept: "*/*",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                authorID: author.authorID,
+                authorName: author.authorName,
+            }),
+        })
+            .then((response) => response.json())
+            .then((json) => {
+                console.log("author add", json);
+                setRecord(json);
+            })
+            .catch(() => onError());
+    };
 
-      <Popup
-        title="Author form"
-        openPopup={openPopup}
-        setOpenPopup={setOpenPopup}
-      >
-        <AuthorForm recordForEdit={recordForEdit} addAuthor={addAuthor} />
-      </Popup>
-      <Notification notify={notify} setNotify={setNotify} />
-    </>
-  );
+    const putAuthor = (author) => {
+        fetch(`https://localhost:7091/Author/Update/${author.authorID}`, {
+            method: "PUT",
+            headers: {
+                accept: "*/*",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                authorID: author.authorID,
+                authorName: author.authorName,
+            }),
+        })
+            .then((response) => response.json())
+            .then((json) => {
+                console.log("author update", json);
+                setRecordForEdit(json);
+            })
+            .catch((e) => console.log(e));
+    };
+
+    const addAuthor = (author, resetForm) => {
+        if (author.authorID == 0) {
+            postAuthor(author);
+        } else {
+            putAuthor(author);
+        }
+        resetForm();
+        setRecordForEdit(null);
+        setOpenPopup(false);
+        setNotify({
+            isOpen: true,
+            message: "Submitted Successfully",
+            type: "success",
+        });
+    };
+
+    useEffect(() => {
+        fetchAuthor();
+    }, [record, recordForEdit]);
+
+    const openInPopup = (item) => {
+        setRecordForEdit(item);
+        setOpenPopup(true);
+    };
+
+    return (
+        <>
+            <div className="datatable">
+                <div className="title">
+                    List Author
+                    <Button
+                        text="Add New"
+                        variant="outlined"
+                        startIcon={<AddIcon />}
+                        size="small"
+                        onClick={() => {
+                            setOpenPopup(true);
+                            //setRecordForEdit(null);
+                        }}
+                        color="addNewAuthor"
+                    />
+                </div>
+
+                <TblContainer>
+                    <TblHead />
+                    <TableBody>
+                        {recordsAfterPagingAndSorting().map((item) => (
+                            <TableRow key={item.authorID} className="rowAuthor">
+                                <TableCell>{item.authorID}</TableCell>
+                                
+                                <TableCell>{item.authorName}</TableCell>
+                                <TableCell className="action">
+                                    <ActionButton
+                                        color="edit"
+                                        onClick={() => {
+                                            openInPopup(item);
+                                        }}
+                                    >
+                                        Edit
+                                    </ActionButton>
+                                    <ActionButton color="delete">Delete</ActionButton>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </TblContainer>
+                <TblPagination className="pagination" />
+            </div>
+
+            <Popup title="Author form" openPopup={openPopup} setOpenPopup={setOpenPopup}>
+                <AuthorForm recordForEdit={recordForEdit} addAuthor={addAuthor} />
+            </Popup>
+            <Notification notify={notify} setNotify={setNotify} />
+        </>
+    );
 };
 
 export default TableAuthor;
--- src/components/datatable/datatableCustomer/DatatableCus.jsx
+++ src/components/datatable/datatableCustomer/DatatableCus.jsx
@@ -1,62 +1,125 @@
-import {useState, useEffect, useMemo, useCallback, useRef} from 'react'
-import axios from 'axios';
-import { filter } from 'lodash';
-import { styled } from '@mui/material/styles';
-import './style/datatableCus.scss'
-import { DataGrid } from '@mui/x-data-grid';
-import 'ag-grid-enterprise';
-import { AgGridReact } from 'ag-grid-react';
-import 'ag-grid-community/dist/styles/ag-grid.css';
-import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
-import { employeeRows, employeeColums } from '../../../data/datatableSource';
-import { Link }  from "react-router-dom"
+import { useState, useEffect, useMemo, useCallback, useRef } from "react";
+import axios from "axios";
+import { filter } from "lodash";
+import { styled } from "@mui/material/styles";
+import "./style/datatableCus.scss";
+import { DataGrid } from "@mui/x-data-grid";
+import "ag-grid-enterprise";
+import { AgGridReact } from "ag-grid-react";
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-alpine.css";
+import { employeeRows, employeeColums } from "../../../data/datatableSource";
+import { Link } from "react-router-dom";
 
 // material
-import {
-    Stack,
-    Button,
-    Typography,
-  } from '@mui/material';
-
+import { Stack, Button, Typography } from "@mui/material";
 
 const DatatableCus = () => {
+    const [page, setPage] = useState(0);
 
-    const actionColumn = [
-        {field: "action" , headerName:"Action", width:220, 
-            renderCell: () => {
-                return (
-                    <div className='cellAction'>
-                        <Link to="test" style={{textDecoration:"none"}}>
-                            <div className='viewButton'>View</div>
+    const [order, setOrder] = useState("asc");
 
-                        </Link>
-                        <div className='deleteButton'>Delete</div>
+    const [selected, setSelected] = useState([]);
 
-                    </div>
-                )
-            }
-        }
-    ]
+    const [orderBy, setOrderBy] = useState("name");
 
-  return (
-    <div className='datatable'>
-        <div className='datatableTitle'>
-            List Customers
-            {/* <Link to="new" style={{textDecoration: "none"}} className="link">
-                Add New
-            </Link> */}
+    const [filterName, setFilterName] = useState("");
+
+    const [paginationPageSize, setPaginationPageSize] = useState(10);
+
+    const [rowsPerPage, setRowsPerPage] = useState(5);
+
+    // const [vouchers, setVoucher] = useState([]);
+
+    const [error, setError] = useState(null);
+
+    const defaultColDef = useMemo(() => {
+        return {
+            flex: 1,
+            minWidth: 150,
+            filter: true,
+        };
+    }, []);
+
+    const containerStyle = useMemo(() => ({ width: "100%", height: "100%" }), []);
+    const gridStyle = useMemo(() => ({ height: "100%", width: "100%" }), []);
+    const [rowData, setRowData] = useState([]);
+
+    const [columnDefs, setColumnDefs] = useState([
+        { field: "accountID" },
+        { field: "accountEmail" },
+        { field: "status" },
+    ]);
+
+    const config = {
+        Headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+    };
+
+    const bodyParameters = {
+        params: {
+            pageNumber: 1,
+            pageSize: paginationPageSize,
+            filterString: "a",
+        },
+    };
+
+    const onGridReady = useCallback((params) => {
+        axios
+            .get("https://localhost:7091/Account/Get", bodyParameters, config)
+            .then((response) => {
+                setRowData(response.data);
+            })
+            .catch((error) => {
+                setError(error);
+            });
+    }, []);
+
+    console.log(rowData);
+
+    const handleFilterByName = (event) => {
+        setFilterName(event.target.value);
+    };
+    // const actionColumn = [
+    //     {field: "action" , headerName:"Action", width:220,
+    //         renderCell: () => {
+    //             return (
+    //                 <div className='cellAction'>
+    //                     <Link to="test" style={{textDecoration:"none"}}>
+    //                         <div className='viewButton'>View</div>
+
+    //                     </Link>
+    //                     <div className='deleteButton'>Delete</div>
+
+    //                 </div>
+    //             )
+    //         }
+    //     }
+    // ]
+
+    return (
+        <div style={containerStyle}>
+            {/* <UserListToolbar
+                filterName={filterName}
+                onFilterName={handleFilterByName}
+                searchName={"Search Customer"}
+            /> */}
+            <div style={gridStyle} className="ag-theme-alpine">
+                <AgGridReact
+                    rowData={rowData}
+                    columnDefs={columnDefs}
+                    defaultColDef={defaultColDef}
+                    //   rowModelType={'serverSide'}
+                    pagination
+                    paginationPageSize={paginationPageSize}
+                    cacheBlockSize={10}
+                    animateRows
+                    onGridReady={onGridReady}
+                />
+            </div>
         </div>
-        <DataGrid
-            rows={employeeRows}
-            columns={employeeColums.concat(actionColumn)}
-            pageSize={7}
-            rowsPerPageOptions={[5]}
-            checkboxSelection
-            
-            
-        />
-    </div>
-  )
-}
+    );
+};
 
-export default DatatableCus
+export default DatatableCus;
