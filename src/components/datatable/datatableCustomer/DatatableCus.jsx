133ad1d059d5ff324e402da6715d6e08fb499bed
tthanhtung92 <55181588+tthanhtung92@users.noreply.github.com>
2022-11-18 03:41:19 +0700

    fix fix

--- src/components/datatable/datatableCustomer/DatatableCus.jsx
+++ src/components/datatable/datatableCustomer/DatatableCus.jsx
@@ -2,7 +2,7 @@ import { Search } from "@mui/icons-material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import LoopIcon from "@mui/icons-material/Loop";
 import PermContactCalendarIcon from "@mui/icons-material/PermContactCalendar";
-import { TableBody, TableCell, TableRow } from "@mui/material";
+import { InputAdornment, TableBody, TableCell, TableRow } from "@mui/material";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 import axios from "axios";
 import { useEffect, useState } from "react";
@@ -16,247 +16,243 @@ import "../datatableCustomer/style/datatableCus.scss";
 import useTable from "../useTable";
 
 const headCells = [
-  { id: "accountID", label: "ID" },
-  { id: "image", label: "Avatar", disableSorting: true },
-  { id: "accountEmail", label: "Email" },
-  { id: "status", label: "Status" },
-  { id: "action", label: "Action", disableSorting: true },
+    { id: "accountID", label: "ID" },
+    { id: "image", label: "Avatar", disableSorting: true },
+    { id: "accountEmail", label: "Email" },
+    { id: "status", label: "Status" },
+    { id: "action", label: "Action", disableSorting: true },
 ];
 
 const DatatableCus = () => {
-  const user = AuthService.getCurrentUser();
-  const [records, setRecords] = useState([]);
-  const [recordForDelete, setRecordForDelete] = useState(null);
-  const [recordStatus, setRecordStatus] = useState();
-  const [changeStatus, setchangeStatus] = useState(true);
-  const [filterFn, setFilterFn] = useState({
-    fn: (items) => {
-      return items;
-    },
-  });
-
-  const [notify, setNotify] = useState({
-    isOpen: false,
-    message: "",
-    type: "",
-  });
-
-  const [confirmDialog, setConfirmDialog] = useState({
-    isOpen: false,
-    title: "",
-    subTitle: "",
-  });
-
-  const navigate = useNavigate();
-
-  const config = {
-    headers: {
-      "Content-Type": "application/json; charset=utf-8",
-      Accept: "application/json",
-      Authorization: "bearer " + user.token,
-    },
-  };
-
-  useEffect(() => {
-    const onGridReady = async () => {
-      try {
-        await axios
-          .get("https://localhost:7091/Account/GetByRole/2", config)
-          .then((response) => {
-            const resData = response.data;
-            setRecords([...records, ...resData]);
-          });
-      } catch (e) {
-        console.log(e);
-      }
-    };
-    onGridReady();
-  }, []);
-
-  console.log(records);
-
-  const changeStatusCustomer = async () => {
-    try {
-      await axios
-        .put(
-          `https://localhost:7091/Account/ChangeStatus?id=${recordStatus}`,
-          { status: changeStatus },
-          config
-        )
-        .then((respone) => {
-          console.log("Status changed", respone.data);
-          window.location.reload();
-        });
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  const deleteCustomer = async () => {
-    try {
-      await axios
-        .delete(
-          `https://localhost:7091/Account/Delete/${recordForDelete}`,
-          config
-        )
-        .then((respone) => {
-          console.log("Customer Delete", respone.data);
-          window.location.reload();
-          setNotify({
-            isOpen: true,
-            message: "Deleted Successfully",
-            type: "error",
-          });
-        });
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  const { TblContainer, TblHead, TblPagination, recordsAfterPagingAndSorting } =
-    useTable(records, headCells, filterFn);
-
-  const handleSearch = (e) => {
-    let target = e.target;
-    setFilterFn({
-      fn: (items) => {
-        if (target.value == "") return items;
-        else
-          return items.filter((x) =>
-            x.accountEmail.toLowerCase().includes(target.value)
-          );
-      },
+    const user = AuthService.getCurrentUser();
+    const [records, setRecords] = useState([]);
+    const [recordForDelete, setRecordForDelete] = useState(null);
+    const [recordStatus, setRecordStatus] = useState();
+    const [changeStatus, setchangeStatus] = useState(true);
+    const [filterFn, setFilterFn] = useState({
+        fn: (items) => {
+            return items;
+        },
     });
-  };
 
-  return (
-    <>
-      <div className="datatableCustomer">
-        <div className="title">List Customer</div>
-        <div className="searchCus">
-          <Input
-            className="searchInput"
-            label="Search Customer"
-            other={{
-              InputProps: {
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <Search />
-                  </InputAdornment>
-                ),
-              },
-            }}
-            onChange={handleSearch}
-          />
-        </div>
+    const [notify, setNotify] = useState({
+        isOpen: false,
+        message: "",
+        type: "",
+    });
 
-        <TblContainer>
-          <TblHead sx={{ height: "40px" }} />
-          <TableBody>
-            {recordsAfterPagingAndSorting().map((item) => (
-              <TableRow key={item.accountID} className="rowCustomer">
-                <TableCell className="cellID">
-                  <div>{item.accountID}</div>
-                </TableCell>
-                <TableCell className="cellImg">
-                  <img
-                    src={
-                      item?.image !== null
-                        ? item?.image
-                        : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
-                    }
-                    alt=""
-                  />
-                </TableCell>
-                <TableCell className="cellEmail">
-                  <div>{item.accountEmail}</div>
-                </TableCell>
+    const [confirmDialog, setConfirmDialog] = useState({
+        isOpen: false,
+        title: "",
+        subTitle: "",
+    });
 
-                <TableCell className="cellStatus">
-                  {item.status === true ? (
-                    <div className="active">Active</div>
-                  ) : (
-                    <div className="disable">Disable</div>
-                  )}
-                </TableCell>
+    const navigate = useNavigate();
 
-                <TableCell className="action">
-                  <div className="tip">
-                    <TooltipComponent
-                      content="Detail Info"
-                      position="BottomCenter"
-                    >
-                      <ActionButton
-                        color="view"
-                        onClick={() => {
-                          navigate(`/customer/${item.accountID}`);
+    const config = {
+        headers: {
+            "Content-Type": "application/json; charset=utf-8",
+            Accept: "application/json",
+            Authorization: "bearer " + user.token,
+        },
+    };
+
+    useEffect(() => {
+        const onGridReady = async () => {
+            try {
+                await axios
+                    .get("https://localhost:7091/Account/GetByRole/2", config)
+                    .then((response) => {
+                        const resData = response.data;
+                        setRecords([...records, ...resData]);
+                    });
+            } catch (e) {
+                console.log(e);
+            }
+        };
+        onGridReady();
+    }, []);
+
+    console.log(records);
+
+    const changeStatusCustomer = async () => {
+        try {
+            await axios
+                .put(
+                    `https://localhost:7091/Account/ChangeStatus?id=${recordStatus}`,
+                    { status: changeStatus },
+                    config
+                )
+                .then((respone) => {
+                    console.log("Status changed", respone.data);
+                    window.location.reload();
+                });
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
+    const deleteCustomer = async () => {
+        try {
+            await axios
+                .delete(`https://localhost:7091/Account/Delete/${recordForDelete}`, config)
+                .then((respone) => {
+                    console.log("Customer Delete", respone.data);
+                    window.location.reload();
+                    setNotify({
+                        isOpen: true,
+                        message: "Deleted Successfully",
+                        type: "error",
+                    });
+                });
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
+    const { TblContainer, TblHead, TblPagination, recordsAfterPagingAndSorting } = useTable(
+        records,
+        headCells,
+        filterFn
+    );
+
+    const handleSearch = (e) => {
+        let target = e.target;
+        setFilterFn({
+            fn: (items) => {
+                if (target.value == "") return items;
+                else
+                    return items.filter((x) => x.accountEmail.toLowerCase().includes(target.value));
+            },
+        });
+    };
+
+    return (
+        <>
+            <div className="datatableCustomer">
+                <div className="title">List Customer</div>
+                <div className="searchCus">
+                    <Input
+                        className="searchInput"
+                        label="Search Customer"
+                        other={{
+                            InputProps: {
+                                startAdornment: (
+                                    <InputAdornment position="start">
+                                        <Search />
+                                    </InputAdornment>
+                                ),
+                            },
                         }}
-                      >
-                        <PermContactCalendarIcon />
-                      </ActionButton>
-                    </TooltipComponent>
-                    <TooltipComponent
-                      content="Change Status"
-                      position="BottomCenter"
-                    >
-                      <ActionButton
-                        onMouseOver={() => {
-                          setRecordStatus(item.accountID);
-                          setchangeStatus(!item.status);
-                        }}
-                        color="changeSta"
-                        onClick={() => {
-                          changeStatusCustomer();
-                        }}
-                      >
-                        <LoopIcon />
-                      </ActionButton>
-                    </TooltipComponent>
-                    {item.status === true ? (
-                      <ActionButton color="disable" disabled={true}>
-                        <DeleteIcon />
-                      </ActionButton>
-                    ) : (
-                      <TooltipComponent
-                        content="Delete"
-                        position="BottomCenter"
-                      >
-                        <ActionButton
-                          onMouseOver={() => {
-                            setRecordForDelete(item.accountID);
-                          }}
-                          color="delete"
-                          disabled={false}
-                          onClick={() => {
-                            setConfirmDialog({
-                              isOpen: true,
-                              title: "Are you sure to delete this record?",
-                              subTitle: "You can't undo this operation",
-                              onConfirm: () => {
-                                deleteCustomer();
-                              },
-                            });
-                          }}
-                        >
-                          <DeleteIcon />
-                        </ActionButton>
-                      </TooltipComponent>
-                    )}
-                  </div>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </TblContainer>
-        <TblPagination className="pagination" />
-        <ConfirmDialog
-          confirmDialog={confirmDialog}
-          setConfirmDialog={setConfirmDialog}
-        />
-        <Notification notify={notify} setNotify={setNotify} />
-      </div>
-    </>
-  );
+                        onChange={handleSearch}
+                    />
+                </div>
+
+                <TblContainer>
+                    <TblHead sx={{ height: "40px" }} />
+                    <TableBody>
+                        {recordsAfterPagingAndSorting().map((item) => (
+                            <TableRow key={item.accountID} className="rowCustomer">
+                                <TableCell className="cellID">
+                                    <div>{item.accountID}</div>
+                                </TableCell>
+                                <TableCell className="cellImg">
+                                    <img
+                                        src={
+                                            item?.image !== null
+                                                ? item?.image
+                                                : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+                                        }
+                                        alt=""
+                                    />
+                                </TableCell>
+                                <TableCell className="cellEmail">
+                                    <div>{item.accountEmail}</div>
+                                </TableCell>
+
+                                <TableCell className="cellStatus">
+                                    {item.status === true ? (
+                                        <div className="active">Active</div>
+                                    ) : (
+                                        <div className="disable">Disable</div>
+                                    )}
+                                </TableCell>
+
+                                <TableCell className="action">
+                                    <div className="tip">
+                                        <TooltipComponent
+                                            content="Detail Info"
+                                            position="BottomCenter"
+                                        >
+                                            <ActionButton
+                                                color="view"
+                                                onClick={() => {
+                                                    navigate(`/customer/${item.accountID}`);
+                                                }}
+                                            >
+                                                <PermContactCalendarIcon />
+                                            </ActionButton>
+                                        </TooltipComponent>
+                                        <TooltipComponent
+                                            content="Change Status"
+                                            position="BottomCenter"
+                                        >
+                                            <ActionButton
+                                                onMouseOver={() => {
+                                                    setRecordStatus(item.accountID);
+                                                    setchangeStatus(!item.status);
+                                                }}
+                                                color="changeSta"
+                                                onClick={() => {
+                                                    changeStatusCustomer();
+                                                }}
+                                            >
+                                                <LoopIcon />
+                                            </ActionButton>
+                                        </TooltipComponent>
+                                        {item.status === true ? (
+                                            <ActionButton color="disable" disabled={true}>
+                                                <DeleteIcon />
+                                            </ActionButton>
+                                        ) : (
+                                            <TooltipComponent
+                                                content="Delete"
+                                                position="BottomCenter"
+                                            >
+                                                <ActionButton
+                                                    onMouseOver={() => {
+                                                        setRecordForDelete(item.accountID);
+                                                    }}
+                                                    color="delete"
+                                                    disabled={false}
+                                                    onClick={() => {
+                                                        setConfirmDialog({
+                                                            isOpen: true,
+                                                            title: "Are you sure to delete this record?",
+                                                            subTitle:
+                                                                "You can't undo this operation",
+                                                            onConfirm: () => {
+                                                                deleteCustomer();
+                                                            },
+                                                        });
+                                                    }}
+                                                >
+                                                    <DeleteIcon />
+                                                </ActionButton>
+                                            </TooltipComponent>
+                                        )}
+                                    </div>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </TblContainer>
+                <TblPagination className="pagination" />
+                <ConfirmDialog confirmDialog={confirmDialog} setConfirmDialog={setConfirmDialog} />
+                <Notification notify={notify} setNotify={setNotify} />
+            </div>
+        </>
+    );
 };
 
 export default DatatableCus;
